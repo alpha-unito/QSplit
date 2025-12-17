0071d213a261a5cf15dec79f361ec93845719211
Mario Bifulco <mario.bifulco@unito.it>
2025-12-17 09:32:52 +0100

    Added license note

--- qsplit/resolve_conflicts.py
+++ qsplit/resolve_conflicts.py
@@ -1,8 +1,24 @@
+# Copyright (C) 2025  The QSplit Contributors.
+# See the 'CONTRIBUTORS' file at the top-level directory of this distribution.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import numpy as np
 import pandas as pd
 
-from qsplit.adapters.dwave.dwave_sa import solve
 from qsplit.adapters.all_zero import solve as solve_zeros
+from qsplit.adapters.dwave.dwave_sa import solve
 from qsplit.qubo import QUBO
 
 
